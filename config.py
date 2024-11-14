3224fcb4b184aa86ebde7ce9effcce40b96b938d
Vittorio Durigutti <121116119+vittoriodurigutti@users.noreply.github.com>
2024-11-13 23:12:15 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+DB_CONFIG = {
+    'host': 'localhost',        # Conectar a la base de datos local
+    'port': 3306,               # Puerto estándar de MySQL
+    'user': 'FP',               # Nombre de usuario de la base de datos
+    'password': 'FP',           # Contraseña del usuario (ajústala según corresponda)
+    'database': 'iot_project_FP'  # Nombre de la base de datos
+}
